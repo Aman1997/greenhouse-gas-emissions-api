aed39406e1d94074e001755522a87e036697af62
Aman <amanb218@gmail.com>
2021-05-09 23:31:42 +0530

    updated apiSpec file

--- index.js
+++ index.js
@@ -88,4 +88,4 @@ app.get("/countries/id", async (req, res) => {
 });
 
 // server liostening on port 3000
-app.listen(3000, () => console.log("Listening on port 3000!"));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running!"));
